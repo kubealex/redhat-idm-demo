0944e58f58717f7c898140b7606f8873558aaf48
kubealex <al.rossi87@gmail.com>
2024-07-16 09:30:08 +0200

    fix

--- lab-setup/terraform/lab_setup.tf
+++ lab-setup/terraform/lab_setup.tf
@@ -2,23 +2,23 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-# module "libvirt_resources" {
-#   source = "./libvirt-resources"
-#   network_domain          = var.network_domain
-#   network_cidr            = var.network_cidr    
-#   network_dnsmasq_options = var.network_dnsmasq_options
-# }
+module "libvirt_resources" {
+  source = "./libvirt-resources"
+  network_domain          = var.network_domain
+  network_cidr            = var.network_cidr    
+  network_dnsmasq_options = var.network_dnsmasq_options
+}
 
 module "idm_instances" {
   source  = "./idm"
-  # depends_on = [ module.libvirt_resources ]
+  depends_on = [ module.libvirt_resources ]
   idm_instance_cloud_image = var.idm_instance_cloud_image
   idm_instance_domain = var.network_domain
 }
 
 module "idm_clients" {
   source  = "./idm-clients"
-  # depends_on = [ module.libvirt_resources ]
+  depends_on = [ module.libvirt_resources ]
   rhel9_instance_cloud_image = var.rhel9_instance_cloud_image  
   client_instance_domain = var.network_domain
 }
